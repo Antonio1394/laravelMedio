a6b129877fb5123de0f16af358b3a07a98a7fbd2
Antonio1394 <antoniorivm1394@gmail.com>
2015-11-28 23:01:21 -0600

    [add] seeder Usuarios

--- app/database/seeds/UsuariosTableSeeder.php
+++ app/database/seeds/UsuariosTableSeeder.php
@@ -5,14 +5,31 @@ class UsuariosTableSeeder extends Seeder {
 	public function run()
 	{
 		// Uncomment the below to wipe the table clean before populating
-		// DB::table('usuarios')->truncate();
+		DB::table('usuarios')->truncate();
 
-		$usuarios = array(
+		$usuarios =  [
+			[
+				'email'=>'thor@thor.com',
+				'password'=>Hash::make('1234'),
+				'nombre'=>'Thor',
+				'apellido'=>'Vengador',
+				'admin'=>0,
+				'created_at'=> new DateTime
+			],
+			[
+				'email'=>'admin@admin.com',
+				'password'=>Hash::make('1234'),
+				'nombre'=>'admin',
+				'apellido'=>'admin',
+				'admin'=>1,
+				
+			],
 
-		);
+
+		];
 
 		// Uncomment the below to run the seeder
-		// DB::table('usuarios')->insert($usuarios);
+		 DB::table('usuarios')->insert($usuarios);
 	}
 
 }
