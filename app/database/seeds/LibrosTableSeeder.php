aca121e820e72dc0fb7057abe00bb216e827feb1
Antonio1394 <antoniorivm1394@gmail.com>
2015-11-29 23:06:14 -0600

    [add] seeder libros

--- app/database/seeds/LibrosTableSeeder.php
+++ app/database/seeds/LibrosTableSeeder.php
@@ -8,13 +8,13 @@ class LibrosTableSeeder extends Seeder {
 		// DB::table('libros')->truncate();
 
 		Libro::create([
-				'titulo'=>
-				'isbn'=>
-				'precio'=>
-				'publicacion'=>
-				'descripcion'=>
-				'autor_id'=>
-				'categoria_id'=>
+				'titulo'=>'Patito feo'
+				'isbn'=>'123'
+				'precio'=>'13.40'
+				'publicacion'=>1
+				'descripcion'=>'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis quod, modi ut! Quae nam deserunt quia repellat ipsam, molestias illum animi, quis exercitationem aliquid harum omnis impedit, error optio corporis!',
+				'autor_id'=>1
+				'categoria_id'=>1
 			]);
 
 		// Uncomment the below to run the seeder
