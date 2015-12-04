0feb6e63964ab951a1853e56fc28a881b6fc9b24
Antonio1394 <antoniorivm1394@gmail.com>
2015-12-03 23:11:13 -0600

    [update] layouts invitado

--- app/views/includes/header_invitado.blade.php
+++ app/views/includes/header_invitado.blade.php
@@ -1,1 +1,16 @@
-<nav class="navbar navbar-default navbar-inverse" role="navigation"></nav>
+<nav class="navbar navbar-default navbar-inverse" role="navigation">
+	<div class="container-fluid">
+		
+		<div class="navbar-header">
+			<button type="button" class="navbar-toogle" data-toogle="collapse" data-target="#bs-example-navbar-collapse-1">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a href="#" class="navbar-brend">Tienda</a>
+		</div>
+		
+	</div>	
+
+</nav>
