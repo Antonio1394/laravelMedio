58c7f816eddac38794286c6a2974d49a9cbb1acd
Antonio1394 <antoniorivm1394@gmail.com>
2015-12-14 23:02:12 -0600

    [add] view admin

--- app/views/includes/header_admin.blade.php
+++ app/views/includes/header_admin.blade.php
@@ -13,17 +13,20 @@
 
 		<div class="collapse navbar-collapse">
 			<ul class="nav navbar-nav">
-				<li><a href="{{URL::to('/')}}" class="navbar-brand"></a>Inicio</li>
-				<li><a href="{{URL::to('carros')}}" class="navbar-brand"></a>Tu carrito</li>
+				<li><a href="{{URL::to('/admin/index')}}" class="navbar-brand"></a>Inicio</li>
 			</ul>
 
 			<ul class="nav navbar-nav navbar-right">
 				<li class="dropdown">
 					<a href="#" class="dropdown-toggle navbar-brand" data-toggle="dropdown">Bienvenido {{Auth::user()->nombre}} <b class="caret"></b></a>
-					<a href="#" class="dropdown-toggle navbar-brand" data-toggle="dropdown">Login</a>
 					<ul class="dropdown-menu" role="menu">
-						<li><a href="{{URL::to('usuario/ordenes')}}"></a>Mis Ordenes</li>
+						<li><a href="{{URL::to('admin/orden/index')}}"></a>Gestion de Ordenes</li>
+						<li><a href="{{URL::to('admin/usuarios/index')}}"></a>Gestion de Usuarios</li>
+						<li><a href="{{URL::to('admin/autor/index')}}"></a>Gestion de Autores</li>
+						<li><a href="{{URL::to('admin/categoria/index')}}"></a>Gestion de Categorias</li>
+						<li><a href="{{URL::to('admin/categoria/index')}}"></a>Gestion de Ordenes</li>
 						<li><a href="{{URL::to('usuario/logout')}}"></a>Cerrar Sesion</li>
+	
 
 					</ul>
 				</li>	
