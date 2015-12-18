2345f35e186295250f686ef4324a5ef49c5de8c1
Antonio1394 <antoniorivm1394@gmail.com>
2015-12-17 23:03:17 -0600

    [update] scripts

--- app/views/includes/script.blade.php
+++ app/views/includes/script.blade.php
@@ -3,8 +3,10 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 
 
-<script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
-<script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+<script src= "https://code.jquery.com/jquery-1.11.3.min.js"></script>
+<script src= "https://code.jquery.com/ui/1.11.4/jquery-ui.min.js"</script>
+<script src="https://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+
 
 <script>
 	@if (Session::has("error"))
