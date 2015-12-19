71f6a18a22b5c2ee6443f0e8e6b08d7119f4bd97
Antonio1394 <antoniorivm1394@gmail.com>
2015-12-18 23:18:18 -0600

    [add] table pivot

--- app/models/Orden.php
+++ app/models/Orden.php
@@ -4,4 +4,11 @@ class Orden extends Eloquent {
 	protected $guarded = array();
 
 	public static $rules = array();
+
+	protected $fillable = ['miembro_id','direccion','total','estado'];
+
+	public function ordenitems(){
+		return $this->belongsToMany('Libro','OrdenLibros','orden_id','libro_id')->whitPivot('cantidad','[recio','total');
+
+	}
 }
--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -1,1 +1,5 @@
 @extends('layout/main_layout')
+
+@section('content')
+
+@stop
